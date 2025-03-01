5400f55bb5989ec32fde238529a202bdec86d295
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2025-03-01 18:02:26 +0530

    Learn JQuery - Event.PreventDefault(), event.capturing, event.stopPropogation()

--- app.js
+++ app.js
@@ -7,14 +7,26 @@
 //     text.classList.toggle('change')
 // })
 
-const ulName = document.querySelector('.ulName');
-const inputText = document.querySelector('.inputText');
-const changeColor = document.querySelector('.changeColor');
+const ulName = document.querySelector(".ulName");
+const inputText = document.querySelector(".inputText");
+const changeColor = document.querySelector(".changeColor");
 
-changeColor.addEventListener('click', function() {
-    const newLi = document.createElement('LI');
-    const liContent = document.createTextNode(inputText.value);
+// changeColor.addEventListener("click", function () {
+//   const newLi = document.createElement("LI");
+//   const liContent = document.createTextNode(inputText.value);
 
-    newLi.appendChild(liContent);
-    ulName.appendChild(newLi);
-})
+//   newLi.appendChild(liContent);
+//   ulName.appendChild(newLi);
+// });
+
+var div = document.querySelector("div");
+var button = document.querySelector("button");
+
+div.addEventListener("click", () => {
+  console.log("clicked Div");
+});
+
+button.addEventListener("click", (event) => {
+  event.stopPropagation();
+  console.log("clicked button");
+});
